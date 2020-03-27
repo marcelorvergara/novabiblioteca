5edfd6cea5fff9930be0b731b0872b8707b459f3
Marcelo <marcelorv@gmail.com>
2020-03-26 22:29:53 -0300

    Live Livedata live observer não funciona mesmo mesmo

--- app/src/main/java/android/vergara/tafoda/HomeActivity.kt
+++ app/src/main/java/android/vergara/tafoda/HomeActivity.kt
@@ -1,19 +1,15 @@
 package android.vergara.tafoda
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.vergara.tafoda.Model.Note
 import android.vergara.tafoda.ViewModel.LivroViewModel
-import android.vergara.tafoda.frags.HomeFrag
-import android.widget.Toast
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.fragment_login.*
+
 
 class HomeActivity : AppCompatActivity() {
 
@@ -21,11 +17,8 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
+        //rotação
         if (savedInstanceState == null) {
-            //rotação
-
-
             //ViewModel
             var livroViewModel = ViewModelProviders.of(this)[LivroViewModel::class.java]
             //livroViewModel.livro = notes()
@@ -34,8 +27,6 @@ class HomeActivity : AppCompatActivity() {
 
         botnNav.setupWithNavController(findNavController(R.id.navFrag))
     }
-
-
 
 
 }
